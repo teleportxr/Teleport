teleportxr/Teleport

commit fc4ff6cb6774c86e4acd47e52bf5cc8650a79d17

Added ClientSettings for client-specific settings, e.g. different layout of video texture per-client.

SimulCasterRenderer/src/crossplatform/SessionClient.cpp
@@ -11,8 +11,7 @@
 #include "Log.h"
 
 SessionClient::SessionClient(SessionCommandInterface* commandInterface, std::unique_ptr<DiscoveryService>&& discoveryService)
-	: mCommandInterface(commandInterface), discoveryService(std::move(discoveryService)),
-	  timeSinceLastServerComm(0)
+	: mCommandInterface(commandInterface), discoveryService(std::move(discoveryService))
 {}
 
 SessionClient::~SessionClient()
@@ -74,7 +73,6 @@ bool SessionClient::Connect(const ENetAddress& remote, uint timeout)
 		enet_address_get_host_ip(&mServerEndpoint, remote_ip, sizeof(remote_ip));
 		LOG("Connected to session server: %s:%d", remote_ip, remote.port);
 		remoteIP=remote_ip;
-		timeSinceLastServerComm = 0;
 		return true;
 	}
 
@@ -136,8 +134,7 @@ void SessionClient::Disconnect(uint timeout)
 
 	handshakeAcknowledged = false;
 	receivedInitialPos = false;
-	// Client id is created once in discovery service constructor and is retained there
-	// Below value will be always set to same value when a connection is made
+	// TODO: retain client id for reconnection.
 	clientID=0;
 	discovered=false;
 }
@@ -164,8 +161,7 @@ void SessionClient::Frame(const avs::DisplayInfo &displayInfo
 	,const avs::Pose &originPose
 	,const ControllerState* controllerStates
 	,bool requestKeyframe
-	,double t
-	,double deltaTime)
+	,double t)
 {
 	time=t;
 	if(mClientHost && mServerPeer)
@@ -199,22 +195,12 @@ void SessionClient::Frame(const avs::DisplayInfo &displayInfo
 				case ENET_EVENT_TYPE_CONNECT:
 					return;
 				case ENET_EVENT_TYPE_RECEIVE:
-					timeSinceLastServerComm = 0;
 					DispatchEvent(event);
 					break;
 				case ENET_EVENT_TYPE_DISCONNECT:
-					timeSinceLastServerComm = 0;
 					Disconnect(0);
 					return;
 			}
-		}
-
-		timeSinceLastServerComm += deltaTime;
-
-		// Handle cases where we set a breakpoint and enet doesn't receive disconnect message
-		if (timeSinceLastServerComm > (setupCommand.idle_connection_timeout * 0.001f) + 2)
-		{
-			Disconnect(0);
 		}
 	}
 	mPrevControllerState = controllerStates[0];

SimulCasterRenderer/src/crossplatform/SessionClient.h
@@ -59,8 +59,7 @@ public:
         , const avs::Pose &originPose
         , const ControllerState* controllerState
         , bool requestKeyframe
-	    , double time
-        , double deltaTime);
+	    , double time);
 
     bool IsConnected() const;
     bool HasDiscovered() const;
@@ -125,5 +124,4 @@ private:
     bool discovered=false;
 	avs::SetupCommand setupCommand;
 	std::string remoteIP;
-    float timeSinceLastServerComm;
 };

SimulCasterServer/src/SimulCasterServer/ClientMessaging.cpp
@@ -252,31 +252,21 @@ namespace SCServer
 		}
 
 		//Send latest input to managed code for this networking tick; we need the variables as we can't take the memory address of an rvalue.
-		const avs::InputEventBinary* binaryEventsPtr = newBinaryEvents[0].data();
-		const avs::InputEventAnalogue* analogueEventsPtr = newAnalogueEvents[0].data();
-		const avs::InputEventMotion* motionEventsPtr = newMotionEvents[0].data();
-		processNewInput(clientID, &newInputState[0], &binaryEventsPtr, &analogueEventsPtr, &motionEventsPtr);
+		const avs::InputEventBinary* binaryEventsPtr = newInputStateAndEvents[0].newBinaryEvents.data();
+		const avs::InputEventAnalogue* analogueEventsPtr = newInputStateAndEvents[0].newAnalogueEvents.data();
+		const avs::InputEventMotion* motionEventsPtr = newInputStateAndEvents[0].newMotionEvents.data();
+		processNewInput(clientID, &newInputStateAndEvents[0].inputState, &binaryEventsPtr, &analogueEventsPtr, &motionEventsPtr);
 
 		//Input has been passed, so clear the events.
-		newInputState[0].binaryEventAmount = 0;
-		newInputState[0].analogueEventAmount = 0;
-		newInputState[0].motionEventAmount = 0;
-		newBinaryEvents[0].clear();
-		newAnalogueEvents[0].clear();
-		newMotionEvents[0].clear();
+		newInputStateAndEvents[0].clear();
 
-		binaryEventsPtr = newBinaryEvents[1].data();
-		analogueEventsPtr = newAnalogueEvents[1].data();
-		motionEventsPtr = newMotionEvents[1].data();
-		processNewInput(clientID, &newInputState[1], &binaryEventsPtr, &analogueEventsPtr, &motionEventsPtr);
+		binaryEventsPtr = newInputStateAndEvents[1].newBinaryEvents.data();
+		analogueEventsPtr = newInputStateAndEvents[1].newAnalogueEvents.data();
+		motionEventsPtr = newInputStateAndEvents[1].newMotionEvents.data();
+		processNewInput(clientID, &newInputStateAndEvents[1].inputState, &binaryEventsPtr, &analogueEventsPtr, &motionEventsPtr);
 
 		//Input has been passed, so clear the events.
-		newInputState[1].binaryEventAmount = 0;
-		newInputState[1].analogueEventAmount = 0;
-		newInputState[1].motionEventAmount = 0;
-		newBinaryEvents[1].clear();
-		newAnalogueEvents[1].clear();
-		newMotionEvents[1].clear();
+		newInputStateAndEvents[1].clear();
 
 		// We may stop debugging on client and not receive an ENET_EVENT_TYPE_DISCONNECT so this should handle it. 
 		if (host && peer && timeSinceLastClientComm > (disconnectTimeout / 1000.0f) + 2)
@@ -552,30 +542,31 @@ namespace SCServer
 		}
 
 		uint32_t controllerID = inputState.controllerId;
-		inputState.binaryEventAmount += newInputState[controllerID].binaryEventAmount;
-		inputState.analogueEventAmount += newInputState[controllerID].analogueEventAmount;
-		inputState.motionEventAmount += newInputState[controllerID].motionEventAmount;
-		newInputState[controllerID] = inputState;
+		auto &newInputState=newInputStateAndEvents[controllerID];
+		inputState.binaryEventAmount += newInputState.inputState.binaryEventAmount;
+		inputState.analogueEventAmount += newInputState.inputState.analogueEventAmount;
+		inputState.motionEventAmount += newInputState.inputState.motionEventAmount;
+		newInputState.inputState = inputState;
 
-		if(newInputState[controllerID].binaryEventAmount != 0)
+		if(newInputState.inputState.binaryEventAmount != 0) 
 		{
-			size_t oldBinarySize = sizeof(avs::InputEventBinary) * newBinaryEvents[controllerID].size();
-			newBinaryEvents[controllerID].resize(newBinaryEvents[controllerID].size() + inputState.binaryEventAmount);
-			memcpy(newBinaryEvents[controllerID].data() + oldBinarySize, packet->data + inputStateSize, binaryEventSize);
+			size_t oldBinarySize = sizeof(avs::InputEventBinary) * newInputState.newBinaryEvents.size();
+			newInputState.newBinaryEvents.resize(newInputState.newBinaryEvents.size() + inputState.binaryEventAmount);
+			memcpy(newInputState.newBinaryEvents.data() + oldBinarySize, packet->data + inputStateSize, binaryEventSize);
 		}
 
-		if(newInputState[controllerID].analogueEventAmount != 0)
+		if(newInputState.inputState.analogueEventAmount != 0)
 		{
-			size_t oldAnalogueSize = sizeof(avs::InputEventAnalogue) * newAnalogueEvents[controllerID].size();
-			newAnalogueEvents[controllerID].resize(newAnalogueEvents[controllerID].size() + inputState.analogueEventAmount);
-			memcpy(newAnalogueEvents[controllerID].data() + oldAnalogueSize, packet->data + inputStateSize + binaryEventSize, analogueEventSize);
+			size_t oldAnalogueSize = sizeof(avs::InputEventAnalogue) * newInputState.newAnalogueEvents.size();
+			newInputState.newAnalogueEvents.resize(newInputState.newAnalogueEvents.size() + inputState.analogueEventAmount);
+			memcpy(newInputState.newAnalogueEvents.data() + oldAnalogueSize, packet->data + inputStateSize + binaryEventSize, analogueEventSize);
 		}
 
-		if(newInputState[controllerID].motionEventAmount != 0)
+		if(newInputState.inputState.motionEventAmount != 0)
 		{
-			size_t oldMotionSize = sizeof(avs::InputEventMotion) * newMotionEvents[controllerID].size();
-			newMotionEvents[controllerID].resize(newMotionEvents[controllerID].size() + inputState.motionEventAmount);
-			memcpy(newMotionEvents[controllerID].data() + oldMotionSize, packet->data + inputStateSize + binaryEventSize + analogueEventSize, motionEventSize);
+			size_t oldMotionSize = sizeof(avs::InputEventMotion) * newInputState.newMotionEvents.size();
+			newInputState.newMotionEvents.resize(newInputState.newMotionEvents.size() + inputState.motionEventAmount);
+			memcpy(newInputState.newMotionEvents.data() + oldMotionSize, packet->data + inputStateSize + binaryEventSize + analogueEventSize, motionEventSize);
 		}
 	}
 

SimulCasterServer/src/SimulCasterServer/ClientMessaging.h
@@ -135,12 +135,25 @@ namespace SCServer
 		std::vector<avs::uid> nodesEnteredBounds;	//Stores nodes client needs to know have entered streaming bounds.
 		std::vector<avs::uid> nodesLeftBounds;		//Stores nodes client needs to know have left streaming bounds.
 
-		avs::InputState newInputState[2]; //Newest input state received from the client.
+		struct InputStateAndEvents
+		{
+			void clear()
+			{
+				inputState.binaryEventAmount = 0;
+				inputState.analogueEventAmount = 0;
+				inputState.motionEventAmount = 0;
+				newBinaryEvents.clear();
+				newAnalogueEvents.clear();
+				newMotionEvents.clear();
+			}
+			avs::InputState inputState;
+			//New input events we have received from the client this tick.
+			std::vector<avs::InputEventBinary> newBinaryEvents;
+			std::vector<avs::InputEventAnalogue> newAnalogueEvents;
+			std::vector<avs::InputEventMotion> newMotionEvents;
+		};
+		InputStateAndEvents newInputStateAndEvents[2]; //Newest input state received from the client.
 
-		//New input events we have received from the client this tick.
-		std::vector<avs::InputEventBinary> newBinaryEvents[2];
-		std::vector<avs::InputEventAnalogue> newAnalogueEvents[2];
-		std::vector<avs::InputEventMotion> newMotionEvents[2];
 
 		void dispatchEvent(const ENetEvent& event);
 		void receiveHandshake(const ENetPacket* packet);

client/TeleportVRQuestClient/Src/ClientRenderer.cpp
@@ -832,54 +832,50 @@ void ClientRenderer::RenderLocalNodes(OVRFW::ovrRendererOutput &res)
 
 void ClientRenderer::RenderNode(OVRFW::ovrRendererOutput &res, std::shared_ptr<scr::Node> node)
 {
-	if(node->IsVisible())
+	std::shared_ptr<OVRNode> ovrNode = std::static_pointer_cast<OVRNode>(node);
+
+	//Get final transform.
+	scr::mat4 globalMatrix = node->GetGlobalTransform().GetTransformMatrix();
+	clientDeviceState->transformToLocalOrigin = scr::mat4::Identity();//Translation(-clientDeviceState->localFootPos);
+	scr::mat4 scr_Transform = clientDeviceState->transformToLocalOrigin * globalMatrix;
+
+	//Convert transform to OVR type.
+	OVR::Matrix4f transform;
+	memcpy(&transform.M[0][0], &scr_Transform.a, 16 * sizeof(float));
+
+	//Update skin uniform buffer to animate skinned meshes.
+	std::shared_ptr<scr::Skin> skin = ovrNode->GetSkin();
+	if (skin)
 	{
-		std::shared_ptr<OVRNode> ovrNode = std::static_pointer_cast<OVRNode>(node);
-
-		//Get final transform.
-		scr::mat4 globalMatrix = node->GetGlobalTransform().GetTransformMatrix();
-		clientDeviceState->transformToLocalOrigin = scr::mat4::Identity();//Translation(-clientDeviceState->localFootPos);
-		scr::mat4 scr_Transform = clientDeviceState->transformToLocalOrigin * globalMatrix;
-
-		//Convert transform to OVR type.
-		OVR::Matrix4f transform;
-		memcpy(&transform.M[0][0], &scr_Transform.a, 16 * sizeof(float));
-
-		//Update skin uniform buffer to animate skinned meshes.
-		std::shared_ptr<scr::Skin> skin = ovrNode->GetSkin();
-		if(skin)
-		{
-			skin->UpdateBoneMatrices(globalMatrix);
-		}
-		GlobalGraphicsResources &globalGraphicsResources = GlobalGraphicsResources::GetInstance();
-		std::vector<const scr::ShaderResource *> pbrShaderResources;
-		pbrShaderResources.push_back(&globalGraphicsResources.scrCamera->GetShaderResource());
-		//Push surfaces onto render queue.
-		for(ovrSurfaceDef &surfaceDef : ovrNode->ovrSurfaceDefs)
-		{
-			// Must update the uniforms. e.g. camera pos.
-			for(const scr::ShaderResource *sr : pbrShaderResources)
-			{
-				const std::vector<scr::ShaderResource::WriteShaderResource> &shaderResourceSet = sr->GetWriteShaderResources();
-				int j = 0;
-				for(const scr::ShaderResource::WriteShaderResource &resource : shaderResourceSet)
-				{
-					scr::ShaderResourceLayout::ShaderResourceType type = resource.shaderResourceType;
-					if(type == scr::ShaderResourceLayout::ShaderResourceType::UNIFORM_BUFFER && resource.bufferInfo.buffer)
-					{
-						scc::GL_UniformBuffer *gl_uniformBuffer = static_cast<scc::GL_UniformBuffer *>(resource.bufferInfo.buffer);
-						surfaceDef.graphicsCommand.UniformData[j].Data = &(gl_uniformBuffer->GetGlBuffer());
-					}
-					j++;
-				}
-				OVRFW::GlBuffer &buf = ((scc::GL_ShaderStorageBuffer *)globalGraphicsResources.mTagDataBuffer.get())->GetGlBuffer();
-				surfaceDef.graphicsCommand.UniformData[1].Data = &buf;
-			}
-
-			res.Surfaces.emplace_back(transform, &surfaceDef);
-		}
+		skin->UpdateBoneMatrices(globalMatrix);
 	}
+	GlobalGraphicsResources& globalGraphicsResources = GlobalGraphicsResources::GetInstance();
+	std::vector<const scr::ShaderResource*> pbrShaderResources;
+	pbrShaderResources.push_back(&globalGraphicsResources.scrCamera->GetShaderResource());
+	//Push surfaces onto render queue.
+	for (ovrSurfaceDef &surfaceDef : ovrNode->ovrSurfaceDefs)
+	{
+		// Must update the uniforms. e.g. camera pos.
+		for(const scr::ShaderResource *sr : pbrShaderResources)
+		{
+			const std::vector<scr::ShaderResource::WriteShaderResource> &shaderResourceSet = sr->GetWriteShaderResources();
+			int j=0;
+			for(const scr::ShaderResource::WriteShaderResource& resource : shaderResourceSet)
+			{
+				scr::ShaderResourceLayout::ShaderResourceType type = resource.shaderResourceType;
+				if(type == scr::ShaderResourceLayout::ShaderResourceType::UNIFORM_BUFFER&&resource.bufferInfo.buffer)
+				{
+					scc::GL_UniformBuffer *gl_uniformBuffer = static_cast<scc::GL_UniformBuffer *>(resource.bufferInfo.buffer);
+					surfaceDef.graphicsCommand.UniformData[j].Data = &(gl_uniformBuffer->GetGlBuffer());
+				}
+				j++;
+			}
+			OVRFW::GlBuffer &buf = ((scc::GL_ShaderStorageBuffer *) globalGraphicsResources.mTagDataBuffer.get())->GetGlBuffer();
+			surfaceDef.graphicsCommand.UniformData[1].Data = &buf;
+		}
 
+		res.Surfaces.emplace_back(transform, &surfaceDef);
+	}
 	//Render children.
 	for (std::weak_ptr<scr::Node> childPtr : node->GetChildren())
 	{

pc_client/ClientRenderer.cpp
@@ -281,13 +281,7 @@ void ClientRenderer::ChangePass(ShaderMode newShaderMode)
 void ClientRenderer::Render(int view_id, void* context, void* renderTexture, int w, int h, long long frame)
 {
 	simul::crossplatform::GraphicsDeviceContext	deviceContext;
-	deviceContext.setDefaultRenderTargets(renderTexture,
-		nullptr,
-		0,
-		0,
-		w,
-		h
-	);
+	deviceContext.setDefaultRenderTargets(renderTexture,nullptr,0,0,w,h);
 	static simul::core::Timer timer;
 	static float last_t = 0.0f;
 	timer.UpdateTime();
@@ -322,17 +316,16 @@ void ClientRenderer::Render(int view_id, void* context, void* renderTexture, int
 		vec3 pos = videoPosDecoded ? videoPos : vec3(0, 0, 0);
 		camera.SetPosition(pos);
 	}
-	//relativeHeadPos=camera.GetPosition();
 	// The following block renders to the hdrFramebuffer's rendertarget:
 	//vec3 finalViewPos=localOriginPos+relativeHeadPos;
 	{
 		simul::geometry::SimulOrientation	globalOrientation;
 		// global pos/orientation:
 		globalOrientation.SetPosition((const float*)&clientDeviceState->headPose.position);
-		//globalOrientation.SetPosition({ 0,0,0 });
+	
 		simul::math::Quaternion q0(3.1415926536f/2.0f, simul::math::Vector3(-1.f, 0.0f, 0.0f));
 		simul::math::Quaternion q1 = (const float*)&clientDeviceState->headPose.orientation;
-		//simul::math::Quaternion q_rel = (const float*)&vec4(0,0,0,1);
+	
 		auto q_rel=q1/q0;
 		globalOrientation.SetOrientation(q_rel);
 		deviceContext.viewStruct.view = globalOrientation.GetInverseMatrix().RowPointer(0);
@@ -1159,15 +1152,15 @@ void ClientRenderer::OnVideoStreamChanged(const char *server_ip,const avs::Setup
 	{
 		videoTexture->ensureTextureArraySizeAndFormat(renderPlatform, videoConfig.colour_cubemap_size, videoConfig.colour_cubemap_size, 1, 1,
 			crossplatform::PixelFormat::RGBA_32_FLOAT, true, false, true);
-		specularCubemapTexture->ensureTextureArraySizeAndFormat(renderPlatform, videoConfig.specular_cubemap_size, videoConfig.specular_cubemap_size, 1, videoConfig.specular_mips, crossplatform::PixelFormat::RGBA_8_UNORM, true, false, true);
-		diffuseCubemapTexture->ensureTextureArraySizeAndFormat(renderPlatform, videoConfig.diffuse_cubemap_size, videoConfig.diffuse_cubemap_size, 1, 1,
-			crossplatform::PixelFormat::RGBA_8_UNORM, true, false, true);
 	}
 	else
 	{
 		videoTexture->ensureTextureArraySizeAndFormat(renderPlatform, videoConfig.perspective_width, videoConfig.perspective_height, 1, 1,
 			crossplatform::PixelFormat::RGBA_32_FLOAT, true, false, false);
 	}
+	specularCubemapTexture->ensureTextureArraySizeAndFormat(renderPlatform, videoConfig.specular_cubemap_size, videoConfig.specular_cubemap_size, 1, videoConfig.specular_mips, crossplatform::PixelFormat::RGBA_8_UNORM, true, false, true);
+	diffuseCubemapTexture->ensureTextureArraySizeAndFormat(renderPlatform, videoConfig.diffuse_cubemap_size, videoConfig.diffuse_cubemap_size, 1, 1,
+		crossplatform::PixelFormat::RGBA_8_UNORM, true, false, true);
 
 	const float aspect = setupCommand.video_config.perspective_width / static_cast<float>(setupCommand.video_config.perspective_height);
 	const float horzFOV = setupCommand.video_config.perspective_fov * scr::DEG_TO_RAD;
@@ -1512,7 +1505,7 @@ void ClientRenderer::OnFrameMove(double fTime,float time_step)
 		FillInControllerPose(0,1.0f);
 		FillInControllerPose(1, -1.0f);
 
-		sessionClient.Frame(displayInfo, clientDeviceState->headPose, clientDeviceState->controllerPoses, receivedInitialPos, clientDeviceState->originPose, controllerStates, decoder->idrRequired(),fTime, time_step);
+		sessionClient.Frame(displayInfo, clientDeviceState->headPose, clientDeviceState->controllerPoses, receivedInitialPos, clientDeviceState->originPose, controllerStates, decoder->idrRequired(),fTime);
 
 		for(int i = 0; i < 2; i++)
 		{

SimulCasterAudio/src/crossplatform/NetworkPipeline.cpp
@@ -56,7 +56,7 @@ namespace sca
 			stream.isDataLimitPerFrame = false;
 			stream.counter = 0;
 			stream.chunkSize = 2048;
-			stream.id = 100 + i;
+			stream.id = 100 + (uint32_t)i;
 			stream.dataType = avs::NetworkDataType::Audio;
 			streams.emplace_back(std::move(stream));
 		}

SimulCasterServer/src/SimulCasterServer/CasterSettings.h
@@ -17,9 +17,9 @@ namespace SCServer
 
 		bool enableGeometryStreaming = false;
 		uint32_t geometryTicksPerSecond = 0;
-		int32_t geometryBufferCutoffSize = 0; // Size we stop encoding nodes at.
-		float confirmationWaitTime = 0.0f; // Seconds to wait before resending a resource.
-		float clientDrawDistanceOffset = 0.0f; // Offset for distance pixels are clipped at for geometry on the client.
+		int32_t geometryBufferCutoffSize = 0;	// Size we stop encoding nodes at.
+		float confirmationWaitTime = 0.0f;		// Seconds to wait before resending a resource.
+		float clientDrawDistanceOffset = 0.0f;	// Offset for distance pixels are clipped at for geometry on the client.
 
 		bool enableVideoStreaming = false;
 		bool enableWebcamStreaming = false;
@@ -67,12 +67,27 @@ namespace SCServer
 
 		avs::AxesStandard axesStandard = avs::AxesStandard::NotInitialized;
 
-		int32_t specularCubemapSize = 0;
-		int32_t specularMips = 0;
-		int32_t diffuseCubemapSize = 0;
-		int32_t lightCubemapSize = 0;
+		int32_t defaultSpecularCubemapSize = 0;
+		int32_t defaultSpecularMips = 0;
+		int32_t defaultDiffuseCubemapSize = 0;
+		int32_t defaultLightCubemapSize = 0;
+		int32_t defaultShadowmapSize=0;
 
 		avs::ControlModel controlModel=avs::ControlModel::CLIENT_ORIGIN_SERVER_GRAVITY;
+	};
+	struct ClientSettings
+	{
+		int32_t specularPos[2];
+		int32_t specularCubemapSize ;
+		int32_t specularMips;
+		int32_t diffusePos[2];
+		int32_t diffuseCubemapSize;
+		int32_t lightPos[2];
+		int32_t lightCubemapSize ;
+		int32_t shadowmapPos[2];
+		int32_t shadowmapSize;
+		int32_t webcamPos[2];
+		int32_t webcamSize[2];
 	};
 #pragma pack(pop)
 

commit b43a6dc3c9c7163c0ede9ace94d7ffef1a41365c

Fix regression caused by commit: fc4ff6cb6774c86e4acd47e52bf5cc8650a79d17 Invisible nodes won't be drawn on Android, again.

SimulCasterRenderer/src/crossplatform/SessionClient.cpp
@@ -212,7 +212,7 @@ void SessionClient::Frame(const avs::DisplayInfo &displayInfo
 	//Append resource requests to the request list again, if it has been too long since we sent the request.
 	for(auto sentResource : mSentResourceRequests)
 	{
-		float timeSinceSent = float(time - sentResource.second);
+		double timeSinceSent = time - sentResource.second;
 		if(timeSinceSent > RESOURCE_REQUEST_RESEND_TIME)
 		{
 			SCR_COUT << "Requesting resource " << sentResource.first << " again, as it has been " << timeSinceSent << " seconds since we sent the last request." << std::endl;

SimulCasterRenderer/src/crossplatform/SessionClient.h
@@ -97,7 +97,7 @@ private:
     void ReceiveNodeMovementUpdate(const ENetPacket* packet);
 	void ReceiveNodeAnimationUpdate(const ENetPacket* packet);
 
-	static constexpr float RESOURCE_REQUEST_RESEND_TIME = 10.0f; //Seconds we wait before resending a resource request.
+	static constexpr double RESOURCE_REQUEST_RESEND_TIME = 10.0; //Seconds we wait before resending a resource request.
 
     avs::uid lastServerID = 0; //UID of the server we last connected to.
 

SimulCasterServer/src/SimulCasterServer/ClientMessaging.cpp
@@ -257,15 +257,13 @@ namespace SCServer
 		const avs::InputEventMotion* motionEventsPtr = newInputStateAndEvents[0].newMotionEvents.data();
 		processNewInput(clientID, &newInputStateAndEvents[0].inputState, &binaryEventsPtr, &analogueEventsPtr, &motionEventsPtr);
 
-		//Input has been passed, so clear the events.
-		newInputStateAndEvents[0].clear();
-
 		binaryEventsPtr = newInputStateAndEvents[1].newBinaryEvents.data();
 		analogueEventsPtr = newInputStateAndEvents[1].newAnalogueEvents.data();
 		motionEventsPtr = newInputStateAndEvents[1].newMotionEvents.data();
 		processNewInput(clientID, &newInputStateAndEvents[1].inputState, &binaryEventsPtr, &analogueEventsPtr, &motionEventsPtr);
 
 		//Input has been passed, so clear the events.
+		newInputStateAndEvents[0].clear();
 		newInputStateAndEvents[1].clear();
 
 		// We may stop debugging on client and not receive an ENET_EVENT_TYPE_DISCONNECT so this should handle it. 
@@ -542,7 +540,7 @@ namespace SCServer
 		}
 
 		uint32_t controllerID = inputState.controllerId;
-		auto &newInputState=newInputStateAndEvents[controllerID];
+		InputStateAndEvents& newInputState = newInputStateAndEvents[controllerID];
 		inputState.binaryEventAmount += newInputState.inputState.binaryEventAmount;
 		inputState.analogueEventAmount += newInputState.inputState.analogueEventAmount;
 		inputState.motionEventAmount += newInputState.inputState.motionEventAmount;

SimulCasterServer/src/SimulCasterServer/ClientMessaging.h
@@ -107,6 +107,7 @@ namespace SCServer
 
 	private:
 		static bool asyncNetworkDataProcessingFailed;
+
 		avs::uid clientID;
 		bool initialized=false;
 		bool startingSession;
@@ -153,7 +154,6 @@ namespace SCServer
 			std::vector<avs::InputEventMotion> newMotionEvents;
 		};
 		InputStateAndEvents newInputStateAndEvents[2]; //Newest input state received from the client.
-
 
 		void dispatchEvent(const ENetEvent& event);
 		void receiveHandshake(const ENetPacket* packet);

client/TeleportVRQuestClient/Src/ClientRenderer.cpp
@@ -834,55 +834,59 @@ void ClientRenderer::RenderLocalNodes(OVRFW::ovrRendererOutput &res)
 
 void ClientRenderer::RenderNode(OVRFW::ovrRendererOutput &res, std::shared_ptr<scr::Node> node)
 {
-	std::shared_ptr<OVRNode> ovrNode = std::static_pointer_cast<OVRNode>(node);
-
-	//Get final transform.
-	scr::mat4 globalMatrix = node->GetGlobalTransform().GetTransformMatrix();
-	clientDeviceState->transformToLocalOrigin = scr::mat4::Identity();//Translation(-clientDeviceState->localFootPos);
-	scr::mat4 scr_Transform = clientDeviceState->transformToLocalOrigin * globalMatrix;
-
-	//Convert transform to OVR type.
-	OVR::Matrix4f transform;
-	memcpy(&transform.M[0][0], &scr_Transform.a, 16 * sizeof(float));
-
-	//Update skin uniform buffer to animate skinned meshes.
-	std::shared_ptr<scr::Skin> skin = ovrNode->GetSkin();
-	if (skin)
+	if(node->IsVisible())
 	{
-		skin->UpdateBoneMatrices(globalMatrix);
-	}
-	GlobalGraphicsResources& globalGraphicsResources = GlobalGraphicsResources::GetInstance();
-	std::vector<const scr::ShaderResource*> pbrShaderResources;
-	pbrShaderResources.push_back(&globalGraphicsResources.scrCamera->GetShaderResource());
-	//Push surfaces onto render queue.
-	for (ovrSurfaceDef &surfaceDef : ovrNode->ovrSurfaceDefs)
-	{
-		// Must update the uniforms. e.g. camera pos.
-		for(const scr::ShaderResource *sr : pbrShaderResources)
+		std::shared_ptr<OVRNode> ovrNode = std::static_pointer_cast<OVRNode>(node);
+
+		//Get final transform.
+		scr::mat4 globalMatrix = node->GetGlobalTransform().GetTransformMatrix();
+		clientDeviceState->transformToLocalOrigin = scr::mat4::Identity();//Translation(-clientDeviceState->localFootPos);
+		scr::mat4 scr_Transform = clientDeviceState->transformToLocalOrigin * globalMatrix;
+
+		//Convert transform to OVR type.
+		OVR::Matrix4f transform;
+		memcpy(&transform.M[0][0], &scr_Transform.a, 16 * sizeof(float));
+
+		//Update skin uniform buffer to animate skinned meshes.
+		std::shared_ptr<scr::Skin> skin = ovrNode->GetSkin();
+		if(skin)
 		{
-			const std::vector<scr::ShaderResource::WriteShaderResource> &shaderResourceSet = sr->GetWriteShaderResources();
-			int j=0;
-			for(const scr::ShaderResource::WriteShaderResource& resource : shaderResourceSet)
-			{
-				scr::ShaderResourceLayout::ShaderResourceType type = resource.shaderResourceType;
-				if(type == scr::ShaderResourceLayout::ShaderResourceType::UNIFORM_BUFFER&&resource.bufferInfo.buffer)
-				{
-					scc::GL_UniformBuffer *gl_uniformBuffer = static_cast<scc::GL_UniformBuffer *>(resource.bufferInfo.buffer);
-					surfaceDef.graphicsCommand.UniformData[j].Data = &(gl_uniformBuffer->GetGlBuffer());
-				}
-				j++;
-			}
-			OVRFW::GlBuffer &buf = ((scc::GL_ShaderStorageBuffer *) globalGraphicsResources.mTagDataBuffer.get())->GetGlBuffer();
-			surfaceDef.graphicsCommand.UniformData[1].Data = &buf;
+			skin->UpdateBoneMatrices(globalMatrix);
 		}
+		GlobalGraphicsResources &globalGraphicsResources = GlobalGraphicsResources::GetInstance();
+		std::vector<const scr::ShaderResource *> pbrShaderResources;
+		pbrShaderResources.push_back(&globalGraphicsResources.scrCamera->GetShaderResource());
+		//Push surfaces onto render queue.
+		for(ovrSurfaceDef &surfaceDef : ovrNode->ovrSurfaceDefs)
+		{
+			// Must update the uniforms. e.g. camera pos.
+			for(const scr::ShaderResource *sr : pbrShaderResources)
+			{
+				const std::vector<scr::ShaderResource::WriteShaderResource> &shaderResourceSet = sr->GetWriteShaderResources();
+				int j = 0;
+				for(const scr::ShaderResource::WriteShaderResource &resource : shaderResourceSet)
+				{
+					scr::ShaderResourceLayout::ShaderResourceType type = resource.shaderResourceType;
+					if(type == scr::ShaderResourceLayout::ShaderResourceType::UNIFORM_BUFFER && resource.bufferInfo.buffer)
+					{
+						scc::GL_UniformBuffer *gl_uniformBuffer = static_cast<scc::GL_UniformBuffer *>(resource.bufferInfo.buffer);
+						surfaceDef.graphicsCommand.UniformData[j].Data = &(gl_uniformBuffer->GetGlBuffer());
+					}
+					j++;
+				}
+				OVRFW::GlBuffer &buf = ((scc::GL_ShaderStorageBuffer *)globalGraphicsResources.mTagDataBuffer.get())->GetGlBuffer();
+				surfaceDef.graphicsCommand.UniformData[1].Data = &buf;
+			}
 
-		res.Surfaces.emplace_back(transform, &surfaceDef);
+			res.Surfaces.emplace_back(transform, &surfaceDef);
+		}
 	}
+
 	//Render children.
-	for (std::weak_ptr<scr::Node> childPtr : node->GetChildren())
+	for(std::weak_ptr<scr::Node> childPtr : node->GetChildren())
 	{
 		std::shared_ptr<scr::Node> child = childPtr.lock();
-		if (child)
+		if(child)
 		{
 			RenderNode(res, child);
 		}